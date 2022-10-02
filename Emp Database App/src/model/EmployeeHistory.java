ce518dee93f0310e949cf2972b340539573aa747
Jaykumar-Kotiya <kotiya.j@northeastern.edu>
2022-10-02 18:10:48 -0400

    model employee History java class created

---
+++ Emp Database App/src/model/EmployeeHistory.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jkoti
+ */
+public class EmployeeHistory {
+    
+    private ArrayList<Product> history;
+    
+    public EmployeeHistory() {
+        this.history = new ArrayList<Product>();
+    }
+}
